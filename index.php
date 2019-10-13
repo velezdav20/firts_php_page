071be12faecdfb5c4f739cc9f14b69dbb65c33d4
David Velez <velezdav20@gmail.com>
2019-10-13 02:09:44 -0500

    Basic Code added

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php
-echo '<p>
-Hola Mundo!
-</p>';
+
+echo '<p> Hello World! </p>';
+
+//Local Variables
+$integer = 1;
+$float = 1.5;
+$boolean = true;
+$arrayColores = array('Red', 'Green', 'Blue', 'Yellow');
+$string = "Hello";
+
+//echo is the "print" of PHP
+echo $integer;
+echo $float;
+//If the variable $integer is an integer then add 4
+if(is_int($integer)) {
+  $integer += 4;
+  echo $integer;
+}
+
+
 ?>
